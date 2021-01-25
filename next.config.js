1530c971b1199fc78e7338ae3c52f6dc75478709
Luke Burns <lukemburns@gmail.com>
2021-01-25 01:08:39 -0800

    require next-branch-env

--- next.config.js
+++ next.config.js
@@ -1,37 +1,15 @@
-const nextEnv = require('next-env')
-
 // Configure subdomains for branches, if different
 const subdomains = {
   development: 'dev'
 }
 
-// match prefix convention as vercel for compatibility
-const withNextEnv = nextEnv({
-  serverPrefix: '',
-  staticPrefix: 'NEXT_PUBLIC_',
-  publicPrefix: 'NEXT_PUBLIC_RUNTIME_'
-})
-
 // This configuration does THREE separate things:
 
 // 1. Set branch-specific environment variable overrides
 // Example: if on the development branch, NEXT_PUBLIC_URL_DEVELOPMENT overrides NEXT_PUBLIC_URL
-const branch = process.env.VERCEL_GITHUB_COMMIT_REF || 'local'
-if (branch) {
-  console.log(`${branch}: Processing '${branch}' branch overrides...`)
-  if (branch === 'local') {
-    console.log(`${branch}: Not on Vercel deployment, so you must be local. Branch is considered 'local' for overrides, but you probably won't need this unless it helps you stay organized.`)
-  }
-  const BRANCH = branch.toUpperCase()
-  Object.entries(process.env).forEach(([key, val]) => {
-    let _key
-    if (key.indexOf(`_${BRANCH}`) > -1 && key.indexOf(`_${BRANCH}`) === key.length - `_${BRANCH}`.length) {
-      _key = key.slice(0, key.indexOf(`_${BRANCH}`))
-      process.env[_key] = val
-      console.log(`${key} -> ${_key}`)
-    }
-  })
-}
+const withBranchEnv = require('next-branch-env')()
+const branch = process.env.NEXT_PUBLIC_BRANCH
+console.log(`branch=${branch}`)
 
 // 2. Two options for dealing setting a branch-dependent NEXTAUTH_URL variable for Vercel deployments:
 //    (1) Define NEXTAUTH_URL_{BRANCH} for each branch explicitly in Vercel
@@ -41,22 +19,24 @@ if (!process.env.NEXTAUTH_URL) {
   // Be sure to set NEXT_PUBLIC_HOST to root host (e.g. 'osca.coop' but not 'dev.osca.coop')
   const VERCEL_HOST = process.env.VERCEL_URL ? (new URL(process.env.VERCEL_URL)).host : undefined
   const HOST = process.env.NEXT_PUBLIC_HOST || VERCEL_HOST || `localhost:${process.env.PORT || '3000'}`
-  const PROTOCOL = process.env.PROTOCOL || (branch === 'local' ? 'http' : 'https') // assumes no ssl in local dev environment
+  const PROTOCOL = process.env.PROTOCOL || (branch ? 'https' : 'http') // assumes (1) no branch means localhost and (2) that there's no ssl in local dev environment, so probably better just to set PROTOCOL accordingly
 
   // Set Auth URL based on branch
-  if (branch === 'main' || branch === 'local') {
+  if (branch === 'main' || !branch) {
     process.env.NEXTAUTH_URL = `${PROTOCOL}://${HOST}`
   } else {
     process.env.NEXTAUTH_URL = `${PROTOCOL}://${subdomain(branch)}.${HOST}`
   }
 }
+console.log(`nextauthurl=${process.env.NEXTAUTH_URL}`)
 
 // 3. Set base branch for CMS to current deployment branch, if not set. Otherwise, falls back to 'development'.
 if (!process.env.NEXT_PUBLIC_BASE_BRANCH) {
   process.env.NEXT_PUBLIC_BASE_BRANCH = process.env.VERCEL_GITHUB_COMMIT_REF || 'development'
 }
+console.log(`basebranch=${process.env.NEXT_PUBLIC_BASE_BRANCH}`)
 
-module.exports = withNextEnv()
+module.exports = withBranchEnv()
 
 function subdomain (branch) {
   return subdomains[branch] || branch

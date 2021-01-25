f90b3d5e4f634a7a32b779ee7ebdc066e1c17b6f
Luke Burns <lukemburns@gmail.com>
2021-01-25 06:24:11 -0800

    setting env var explicitly in config makes it available where ive logged it...?

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -11,6 +11,12 @@ const auth0 = Providers.Auth0({
 // by default, auth0 reauthenticates the most recent session, which is either convenient or annoying.
 auth0.authorizationUrl = auth0.authorizationUrl += '&prompt=login'
 
-export default async (req, res) => NextAuth(req, res, { 
-  providers: [ auth0 ]
-})
+export default async (req, res) => {
+  console.log({
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    customVar: process.env.customVar
+  })
+  return NextAuth(req, res, { 
+    providers: [ auth0 ]
+  })
+}

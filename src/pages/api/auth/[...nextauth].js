733ae11ec684ff87170747f7926d631100aa714b
Luke Burns <lukemburns@gmail.com>
2021-01-25 06:57:57 -0800

    tracing...dynamically loading next-auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,24 +2,45 @@ import dynamic from 'next/dynamic'
 // import NextAuth from 'next-auth'
 // import Providers from 'next-auth/providers'
 
-const NextAuth = dynamic(import('next-auth'))
-const Providers = dynamic(import('next-auth/providers'))
-
-const auth0 = Providers.Auth0({
-  clientId: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  domain: process.env.AUTH0_DOMAIN
-})
-
-// show login prompt if logged out.
-// by default, auth0 reauthenticates the most recent session, which is either convenient or annoying.
-auth0.authorizationUrl = auth0.authorizationUrl += '&prompt=login'
+let NextAuth
+let Providers
+let auth0
 
 export default async (req, res) => {
-  console.log({
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL
-  })
-  return NextAuth(req, res, { 
-    providers: [ auth0 ]
-  })
+  if (!NextAuth || !auth0) {
+    import('next-auth').then(n => {
+      NextAuth = n.default
+
+      import('next-auth/providers').then(p => {
+        Providers = p.default
+
+        auth0 = Providers.Auth0({
+          clientId: process.env.AUTH0_CLIENT_ID,
+          clientSecret: process.env.AUTH0_CLIENT_SECRET,
+          domain: process.env.AUTH0_DOMAIN
+        })
+        
+        // show login prompt if logged out.
+        // by default, auth0 reauthenticates the most recent session, which is either convenient or annoying.
+        auth0.authorizationUrl = auth0.authorizationUrl += '&prompt=login'
+  
+        console.log({
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+          dynamic: true
+        })
+  
+        NextAuth(req, res, { 
+          providers: [ auth0 ]
+        })
+      }).catch(err => console.error('error Providers', err))
+    }).catch(err => console.error('error NextAuth', err))
+  } else {
+    console.log({
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+      dynamic: false
+    })
+    return NextAuth(req, res, { 
+      providers: [ auth0 ]
+    })
+  }
 }

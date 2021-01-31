ed0bf411f1124e7383d12c11b6e706b13ebac6ee
negtrooper1 <devon.wellsa@gmail.com>
2021-01-31 15:00:00 -0500

    Added fields to lottery form

--- src/components/LotteryForm.js
+++ src/components/LotteryForm.js
@@ -8,7 +8,7 @@ import {
 import { useState } from 'react'
 // import LotteryPreferenceList from './LotteryPreferenceList';
 
-const LotteryForm = ({onSubmit}) => {
+const LotteryForm = ({ onSubmit }) => {
   const [choices, setChoices] = useState(
     [
       'Harkness Housing',
@@ -40,6 +40,18 @@ const LotteryForm = ({onSubmit}) => {
     return error;
   }
 
+  function validateOCMR(ocmr) {
+    const ocmrRegEx = /^\d{4}$/i;
+
+    let error;
+    if (!number) {
+      error = "OCMR is required"
+    } else if (!ocmrRegEx.test(ocmr)) {
+      error = "Invalid OCMR! 😱" // is this 4 digits max?      
+    }
+    return error;
+  }
+
   return (
     <Flex shadow="md" w="100%" borderRadius="lg" borderWidth="1px" overflow="hidden" direction="column" justifyContent="flex-start" mt={20} alignItems="center" pb={8}>
       <Center mb={4} p={4} w="100%" shadow="sm">
@@ -49,6 +61,7 @@ const LotteryForm = ({onSubmit}) => {
         initialValues={{
           firstName: "",
           lastName: "",
+          OCMR: "",
           tNumber: "",
           preferences: [],
         }}
@@ -57,33 +70,46 @@ const LotteryForm = ({onSubmit}) => {
         {props => (
           <Form>
             <Stack spacing={6}>
-              <Field name="firstName" validate={validateName}>
-                {({ field, form }) => (
-                  <FormControl isInvalid={form.errors.firstName && form.touched.firstName}>
-                    <FormLabel htmlFor="firstName">First Name</FormLabel>
-                    <Input {...field} id="firstName" placeholder="Harkness" />
-                    <FormErrorMessage>{form.errors.firstName}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <Field name="lastName" validate={validateName}>
-                {({ field, form }) => (
-                  <FormControl isInvalid={form.errors.lastName && form.touched.lastName}>
-                    <FormLabel htmlFor="lastName">Last Name</FormLabel>
-                    <Input {...field} id="lastName" placeholder="Forever" />
-                    <FormErrorMessage>{form.errors.lastName}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <Field name="tNumber" validate={validateTNumber}>
-                {({ field, form }) => (
-                  <FormControl isInvalid={form.errors.tNumber && form.touched.tNumber}>
-                    <FormLabel htmlFor="tNumber">T-Number</FormLabel>
-                    <Input {...field} id="tNumber" placeholder="T-0000000" />
-                    <FormErrorMessage>{form.errors.tNumber}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
+              <Stack direction={["column", "row"]} spacing={2}>
+                <Field name="firstName" validate={validateName}>
+                  {({ field, form }) => (
+                    <FormControl isInvalid={form.errors.firstName && form.touched.firstName}>
+                      <FormLabel htmlFor="firstName">First Name</FormLabel>
+                      <Input {...field} id="firstName" placeholder="Harkness" />
+                      <FormErrorMessage>{form.errors.firstName}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="lastName" validate={validateName}>
+                  {({ field, form }) => (
+                    <FormControl isInvalid={form.errors.lastName && form.touched.lastName}>
+                      <FormLabel htmlFor="lastName">Last Name</FormLabel>
+                      <Input {...field} id="lastName" placeholder="Forever" />
+                      <FormErrorMessage>{form.errors.lastName}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+              </Stack>
+              <Stack direction={["column", "row"]} spacing={2}>
+                <Field name="tNumber" validate={validateTNumber}>
+                  {({ field, form }) => (
+                    <FormControl isInvalid={form.errors.tNumber && form.touched.tNumber}>
+                      <FormLabel htmlFor="tNumber">T-Number</FormLabel>
+                      <Input {...field} id="tNumber" placeholder="T-0000000" />
+                      <FormErrorMessage>{form.errors.tNumber}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="OCMR" validate={validateOCMR}>
+                  {({ field, form }) => (
+                    <FormControl isInvalid={form.errors.OCMR && form.touched.OCMR}>
+                      <FormLabel htmlFor="OCMR">OCMR</FormLabel>
+                      <Input {...field} id="OCMR" placeholder="1234" />
+                      <FormErrorMessage>{form.errors.OCMR}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+              </Stack>
               <h1>Coop Preference Ranking: </h1>
               <Field name="preferences[0]">
                 {({ field, form }) => (
@@ -151,7 +177,51 @@ const LotteryForm = ({onSubmit}) => {
                   </FormControl>
                 )}
               </Field>
-              <Button                            
+              <Field name="preferences[6]">
+                {({ field, form }) => (
+                  <FormControl>
+                    <Select {...field} variant="outline" placeholder="7">
+                      {choices.map(choice => (
+                        <option key={choice} value={choice}>{choice}</option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="preferences[7]">
+                {({ field, form }) => (
+                  <FormControl>
+                    <Select {...field} variant="outline" placeholder="8">
+                      {choices.map(choice => (
+                        <option key={choice} value={choice}>{choice}</option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="preferences[8]">
+                {({ field, form }) => (
+                  <FormControl>
+                    <Select {...field} variant="outline" placeholder="9">
+                      {choices.map(choice => (
+                        <option key={choice} value={choice}>{choice}</option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="preferences[9]">
+                {({ field, form }) => (
+                  <FormControl>
+                    <Select {...field} variant="outline" placeholder="10">
+                      {choices.map(choice => (
+                        <option key={choice} value={choice}>{choice}</option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                )}
+              </Field>
+              <Button
                 mt={4}
                 colorScheme="teal"
                 isLoading={props.isSubmitting}
@@ -162,7 +232,7 @@ const LotteryForm = ({onSubmit}) => {
             </Stack>
           </Form>
         )}
-      </Formik>      
+      </Formik>
     </Flex>
   )
 }

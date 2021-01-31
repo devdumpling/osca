28d300bd328421c6894d66b693a5cdfc4e21c004
negtrooper1 <devon.wellsa@gmail.com>
2021-01-31 12:02:02 -0500

    more lottery form styling changes

--- src/pages/lottery.js
+++ src/pages/lottery.js
@@ -61,7 +61,7 @@ class EntrySubmission extends React.Component {
           this.state.time >= start && end >= this.state.time
             ? <Stack align="center" spacing={4}>
               <LotteryForm onSubmit={(data, actions) => enterLottery(data, actions, setEntry)} />
-              <Stack p={5} shadow="md" borderWidth="1px">
+              <Stack p={5} shadow="md" borderWidth="1px" borderRadius="md">
                 <Text m={2} fontSize="lg">The {formatId(lotteryId)} lottery is open for submissions!</Text>
                 <Text color="gray.500" fontWeight="thin" mx={2}><CountDown now={this.state.time} future={end} /> remaining</Text>
               </Stack>
